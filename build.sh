bf37a472710b3019876ea168c441d59ea05ac8fe
Hannes Brennhaeuser <contact@hbrennhaeuser.de>
2026-03-02 23:24:13 +0100

    build: fix multi arch image build with podman

--- build.sh
+++ build.sh
@@ -13,21 +13,34 @@ NET_SNMP_VERSION="5.9.4"
 IMAGE_NAME="docker-snmpd"
 VERSION="${REF}-netsnmp.${NET_SNMP_VERSION}"
 
-PLATFORMS=(
-  "linux/amd64"
-  "linux/arm64"
-)
+PLATFORMS=("linux/amd64" "linux/arm64")
+BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
 
+for PLATFORM in "${PLATFORMS[@]}"; do
+  ARCH="${PLATFORM##*/}"
+  TAG="${IMAGE_NAME}:${VERSION}-${ARCH}"
 
-for platform in "${PLATFORMS[@]}"; do
-  extra_tag_args=()
-  [[ "${REF}" == "latest" ]] && extra_tag_args+=( -t "${IMAGE_NAME}:latest" )
+  echo ""
+  echo "========== Building $TAG ==========="
 
   podman build \
-    --platform "${platform}" \
+    --platform "${PLATFORM}" \
+    -t $TAG \
     --build-arg VERSION="${VERSION}" \
     --build-arg NET_SNMP_VERSION="${NET_SNMP_VERSION}" \
-    --build-arg BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
-    -t "${IMAGE_NAME}:${VERSION}" \
-    "${extra_tag_args[@]}" .
+    --build-arg BUILD_DATE="${BUILD_DATE}" .
 done
+
+
+echo ""
+echo "========== Creating manifest ==========="
+
+podman manifest rm "${IMAGE_NAME}:${VERSION}"
+podman manifest create "${IMAGE_NAME}:${VERSION}"
+
+for PLATFORM in "${PLATFORMS[@]}"; do
+  ARCH="${PLATFORM##*/}"
+  podman manifest add "${IMAGE_NAME}:${VERSION}" "${IMAGE_NAME}:${VERSION}-${ARCH}"
+done
+
+podman tag "${IMAGE_NAME}:${VERSION}" "${IMAGE_NAME}:${REF}"
